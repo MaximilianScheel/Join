8c4ffb9d04f6a6098f5e40d68a7923cdd6ba4071
Beatrice Ambos <beatrice.ambos@web.de>
2023-02-27 21:22:40 +0100

    js for prio buttons

--- Join/js/add_Task.js
+++ Join/js/add_Task.js
@@ -1,6 +1,7 @@
 setURL('http://gruppenarbeit-join-474.developerakademie.net/smallest_backend_ever');
 
 let tasks = [];
+let priority = "low" ;
 
 async function init() {
     await downloadFromServer();
@@ -12,7 +13,63 @@ async function init() {
     console.log(a);
 }
 
-// function addTask() {
+function renderPrioBtnClicked(prio) {
+    return `
+        <span>${prio.charAt(0).toUpperCase() + prio.slice(1)}</span>
+        <img src="assets/img/Prio_${prio}_White.png" width="18px"/>
+    `;
+}
+
+function renderPrioBtnUnclicked(prio) {
+    return `
+        <span>${prio.charAt(0).toUpperCase() + prio.slice(1)}</span>
+        <img src="assets/img/Prio_${prio}.png" width="18px"/>
+    `;
+}
+
+function choosePriority(priority) {
+    let urgent = document.getElementById("urgentBtn");
+    let medium = document.getElementById("mediumBtn");
+    let low = document.getElementById("lowBtn");
+
+    switch (priority) {
+        case "urgent":
+            urgent.style.backgroundColor = "#FF3D00";
+            medium.style.backgroundColor = "#FFF";
+            low.style.backgroundColor = "#FFF";
+            urgent.style.color = "#FFF";
+            medium.style.color = "#000";
+            low.style.color = "#000";
+            urgent.innerHTML = renderPrioBtnClicked("urgent");
+            medium.innerHTML = renderPrioBtnUnclicked("medium");
+            low.innerHTML = renderPrioBtnUnclicked("low");
+            break;
+        case "medium":
+            medium.style.backgroundColor = "#FFA800";
+            urgent.style.backgroundColor = "#FFF";
+            low.style.backgroundColor = "#FFF";
+            urgent.style.color = "#000";
+            medium.style.color = "#FFF";
+            low.style.color = "#000";
+            urgent.innerHTML = renderPrioBtnUnclicked("urgent");
+            medium.innerHTML = renderPrioBtnClicked("medium");
+            low.innerHTML = renderPrioBtnUnclicked("low");
+            break;
+        case "low":
+            low.style.backgroundColor = "#7AE229";
+            medium.style.backgroundColor = "#FFF";
+            urgent.style.backgroundColor = "#FFF";
+            urgent.style.color = "#000";
+            medium.style.color = "#000";
+            low.style.color = "#FFF";
+            urgent.innerHTML = renderPrioBtnUnclicked("urgent");
+            medium.innerHTML = renderPrioBtnUnclicked("medium");
+            low.innerHTML = renderPrioBtnClicked("low");
+            break;
+    }
+}
+
+//  function addTask() {
 //     let description = document.getElementById("description");
 //     let category = document.getElementById("category");   
 //     let title = document.getElementById("title");
@@ -27,4 +84,4 @@ async function init() {
 //     localStorage.setItem("allTasks", allTasksAsString);
 //     console.log(task);
 //     clearValues();  
-// }
+//  }
