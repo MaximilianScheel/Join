2cfcc6aaa2a8c12e0288278965c47d275161a218
Maximilian Scheel <scheel.development@gmail.com>
2023-04-26 17:52:02 +0200

    bugs

--- Join/js/logout.js
+++ Join/js/logout.js
@@ -1,11 +1,3 @@
-/**
- * @fileoverview logout.js is a file to handle the logout functionality.
- */
-function openLogout() {
-    let logoutContainer = document.getElementById("logoutContainer");
-    logoutContainer.style.display = "block";
-}
-
 
 /**
  * Closes the logout container and redirects to index.html
